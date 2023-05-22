313ebee59d361d1d0183bb8692cd11754ce09719
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-05-22 19:58:36 +0530

    Initial commit

---
+++ src/test/java/com/hilltop/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilltop.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
