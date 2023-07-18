67636fc44622ee4799b94553db8f740c9c6dbd44
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-07-18 21:38:28 +0530

    code smell

--- src/main/java/com/hilltop/user/domain/request/UserRequestDto.java
+++ src/main/java/com/hilltop/user/domain/request/UserRequestDto.java
@@ -4,13 +4,13 @@ import com.hilltop.user.enums.UserType;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
 @Getter
 @Setter
 public class UserRequestDto extends LoginRequestDto {
 
     private String name;
     private UserType userType;
+
     @Override
     public boolean isRequiredFieldsAvailable() {
         return isNonEmpty(name) && super.isRequiredFieldsAvailable();
